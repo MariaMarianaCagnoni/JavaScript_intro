ca9bdd47900ae6899a571f9fb9bbeaa3eba8f044
Maria Mariana Cagnoni <mariamariana.cagnoni@gmail.com>
2021-12-09 22:38:25 -0300

    estudo de dados

---
+++ introducao/dados.js
@@ -0,0 +1,16 @@
+var quantidade = 20;
+var preco = 6.4;
+let imposto = 1.7;
+let precoFinal = preco + imposto
+
+console.log(quantidade);
+console.log(10);
+console.log(preco);
+console.log(preco + imposto);
+console.log(precoFinal);
+
+console.log(preco = 8.0);
+
+//a forma moderna de definir uma variavel em Javascript é utilizandoa  palavra "let"
+// pode-se criar uma variavel sem o uso da palavra "var" ou "let",porem não é recomendavel
+//"var" está em desuso.
---
+++ introducao/dados_2.js
@@ -0,0 +1,4 @@
+let preco = 19.9;
+let desconto = 0.4;
+
+console.log(preco*( 1- desconto));
